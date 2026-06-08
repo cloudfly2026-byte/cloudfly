f43cd1cbf0660eef342692c1e55c05ae0edbdd34
Edwin guevara <egbmaster2007@gmail.com>
2026-06-08 18:45:35 -0500

    fix: add missing PathVariable import in RbacController

--- backend_new/src/main/java/com/app/controllers/RbacController.java
+++ backend_new/src/main/java/com/app/controllers/RbacController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
