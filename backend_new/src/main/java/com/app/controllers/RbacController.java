80fce8ba04c955472297b803ce3bd7e5bf903af1
Edwin guevara <egbmaster2007@gmail.com>
2026-06-08 18:43:19 -0500

    fix: add missing DeleteMapping import in RbacController

--- backend_new/src/main/java/com/app/controllers/RbacController.java
+++ backend_new/src/main/java/com/app/controllers/RbacController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
